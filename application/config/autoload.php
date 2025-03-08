172a14262b120b800f0f6b954e048ed0f1221347
Nitin Johnson <nitin.johnson2000@gmail.com>
2025-03-08 22:54:10 +0530

    models autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('AppModel', 'Crud');
+$autoload['model'] = array('Appmodel', 'Crud');
